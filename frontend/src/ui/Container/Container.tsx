b931656ca80e17071ecb43818651dbbb6d438c6d
jprice <jprice@izeni.com>
2025-04-21 11:19:51 -0600

    Add "fluid" prop to Container component

--- frontend/src/ui/Container/Container.tsx
+++ frontend/src/ui/Container/Container.tsx
@@ -2,8 +2,10 @@ import clsx from 'clsx';
 import React from 'react';
 import './Container.scss';
 
-export type ContainerProps = React.HTMLAttributes<HTMLDivElement> & {};
+export type ContainerProps = React.HTMLAttributes<HTMLDivElement> & {
+  fluid?: boolean;
+};
 
-export const Container = ({ className, ...rest }: ContainerProps) => {
-  return <div className={clsx('Container', className)} {...rest} />;
+export const Container = ({ className, fluid = false, ...rest }: ContainerProps) => {
+  return <div className={clsx('Container', { 'Container--fluid': fluid }, className)} {...rest} />;
 };
