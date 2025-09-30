016d5051e6470efe0acd82fec2024511618821f2
jprice <jprice@izeni.com>
2025-09-30 13:20:50 -0600

    Create schedule type and frontend model

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,6 +4,7 @@ export const AppointmentModel = new BaseModel<Appointment>('/api/appointments/')
 export const AvailabilityModel = new BaseModel<Availability>('/api/availabilities/');
 export const BlockModel = new BaseModel<Block>('/api/blocks/');
 export const ClientModel = new BaseModel<Client>('/api/clients/');
+export const ScheduleModel = new BaseModel<Schedule>('/api/schedules/');
 export const TechnicianModel = new BaseModel<Technician>('/api/technicians/');
 export const TherapyAppointmentModel = new BaseModel<TherapyAppointment>('/api/therapy-appointments/');
 export const UserModel = new BaseModel<User>('/api/users/');
